46817b4d52ad362267696240feacc5d0affcacc7
lsy969999 <lsy969999@gmail.com>
2024-01-09 19:02:12 +0900

    fill aios

---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by SY L on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
